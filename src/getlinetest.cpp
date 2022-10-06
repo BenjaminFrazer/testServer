e52d5a40642b50135f2991435ad755f3ac7b2cc3
benjaminf <benjamin.frazer.1997@gmail.com>
2022-10-06 15:00:03 +0100

    deleted test files

--- src/getlinetest.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(int argc, char *argv[]) {
-    int in;
-    cout << "enter an int\n";
-    cin >> in;
-    cout << "you entered:" << in <<"\n";
-    return 0;
-}
